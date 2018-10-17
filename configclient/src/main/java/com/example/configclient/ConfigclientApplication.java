db6b039873458d64b7d09fba0b9ff1af975bac33
sunsong <805459487@qq.com>
2018-10-17 17:21:28 +0800

    learn springcloud

---
+++ configclient/src/main/java/com/example/configclient/ConfigclientApplication.java
@@ -0,0 +1,34 @@
+package com.example.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RefreshScope
+public class ConfigclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigclientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
+	}
+
+	@RequestMapping(value = "/hi2")
+	public String hi2(){
+		return foo + "2";
+	}
+}
+
